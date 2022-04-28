f079d9d593fe5c23e9cfcf25ceb1dfb9d0b1cad3
Chris Chicas <christianchicas7@gmail.com>
2022-04-28 18:50:10 -0400

    added forms for login and singup

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,20 @@
 // Component for the sign up screen
+import { Form, Button } from "react-bootstrap"
+
 export default function SignUp(){
     return(
-        <div>
-            
+        <div className="center">
+            <Form>
+                <Form.Group className="mb-3" controlId="formUserName">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" placeholder="Enter username"/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formPassword">
+                    <Form.Check.Label>Password</Form.Check.Label>
+                    <Form.Control type="password" placeholder="Enter password"/>
+                </Form.Group>
+                <Button variant="primary" type="submit">Submit</Button>
+            </Form>    
         </div>
     )
 }
