0f5569281e57c9725049f78ea162c92db799b25e
Chris Chicas <christianchicas7@gmail.com>
2022-05-05 18:39:22 -0400

    modal attempts

--- src/components/NoteCards.js
+++ src/components/NoteCards.js
@@ -1,15 +1,16 @@
 // Component meant to fit into BinderView.js, displaying Notes and a portion of their contents as cards.
 
 //imports
-import {React, useState, useEffect} from 'react';
+import {React, useState, useEffect, Fragment} from 'react';
 import {Card, Button, Container} from 'react-bootstrap';
 import CreateNote from './CreateNote'
-import EditNote from './EditNote';
 
 //function
 export default function NoteCards(props){
     const [notes, setNotes] = useState([])
     const [update, setUpdate] = useState(false)
+    const [description, setDescription] = useState({})
+
     let cards
     if (notes){
         cards = notes.map((note, index) => {
@@ -19,7 +20,7 @@ export default function NoteCards(props){
                 <Card.Body>
                     {note.noteContent}
                     <div style={{marginTop: "5px"}}>
-                        <Button style={{marginRight: "5px"}} className="mb-3" size="sm" variant="primary">Edit</Button>
+                        <Button style={{marginRight: "5px"}} className="mb-3" size="sm" variant="primary" onClick={() => updateDescription(note.noteId, note.noteContent)}>Edit</Button>
                         <Button className="mb-3" size="sm" variant="danger" onClick={() => deleteNote(note.noteId)}>Delete</Button>   
                     </div>
                 </Card.Body>
@@ -46,6 +47,81 @@ export default function NoteCards(props){
         })
         setUpdate(!update)
     }
+
+  //edit description function
+  const updateDescription = async (noteId, noteContent) => {
+      const response = await fetch(`https://binder-io-api.herokuapp.com/notes/${noteId}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        //   body: JSON.stringify({
+        //     noteContent: description,
+        //     updatedAt : date.toUTCString()
+        //   })
+        });
+        const resData = await response.json()
+        setDescription(resData.noteContent)
+      return (
+        <Fragment>
+          <button
+            type="button"
+            class="btn btn-warning"
+            data-toggle="modal"
+            data-target={`#noteId${resData.noteId}`}
+          >
+            Edit
+          </button>
+          <div
+            class="modal"
+            id={`noteId${noteId}`}
+            onClick={() => setDescription(resData.noteContent)}
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h4 class="modal-title">Edit Note</h4>
+                  <button
+                    type="button"
+                    class="close"
+                    data-dismiss="modal"
+                    onClick={() => setDescription(resData.noteContent)}
+                  >
+                    &times;
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <input
+                    type="text"
+                    className="form-control"
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                  />
+                </div>
+                <div class="modal-footer">
+                  <button
+                    type="button"
+                    class="btn btn-warning"
+                    data-dismiss="modal"
+                    onClick={e => updateDescription(e)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-danger"
+                    data-dismiss="modal"
+                    onClick={() => setDescription(resData.noteContent)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Fragment>
+      );
+  };
+  
 
     return (
            <Container>
