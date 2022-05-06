d01e3cc33ecb625ffdfa34e87acd8cca7bd8a1db
Chris Chicas <christianchicas7@gmail.com>
2022-05-05 20:19:30 -0400

    added home page style

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,14 @@
 import { Alert } from "react-bootstrap";
+import BinderImg from "../freebinder.png"
 
 export default function Home(props){
     return(
-        <div>       
+        <div className="center">       
             {props.loggedOut ? <Alert variant="info" onClose={() => props.setLoggedOut(false)} dismissible>Successfully logged out.</Alert> : null}
-            <h2>Home</h2>
+            <h1 >Welcome to Binder.io!</h1>
+            <img src={BinderImg} alt="free img of binder from https://creazilla.com/nodes/1631862-binder-clipart" style={{width: "25vw", marginBottom: "5px"}}/>
+            <h3>A free note taking app! Log in or Sign up to begin!</h3>
         </div>
+        
     )
 }
